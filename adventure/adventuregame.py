b26a14c60a16ce55b9a92016720ff4106991eaf7
patrickvando <patrickvando@gmail.com>
2023-10-31 05:58:10 -0700

    Update how logfile is read

--- adventure/adventuregame.py
+++ adventure/adventuregame.py
@@ -2,6 +2,8 @@ import io
 import re
 import pexpect
 
+PROMPT_MARKER = '\r\n> '
+
 class AdventureGame:
     process: pexpect.spawn
     poffset: int
@@ -20,7 +22,6 @@ class AdventureGame:
 
         self.process = pexpect.spawn('adventure', encoding='utf-8')
         self.process.logfile_read = io.StringIO()
-        self._expect_multiple('>')
         self._update_history()
 
         self.disallowed = ["quit", "save"] # disallow commands that would exit gameplay
@@ -34,19 +35,19 @@ class AdventureGame:
         self.history.seek(self.hoffset)
         h = self.history.read()
         self.hoffset = self.history.tell()
-        if len(h) > 4 and h[-4:] == '\r\n> ': # make output more readable by excluding the next prompt
-            self.hoffset -= 2 # include next prompt in the next read, but not the newlines
-            return h[:-4]
+        if len(h) > len(PROMPT_MARKER) and h[-len(PROMPT_MARKER):] == PROMPT_MARKER: # make output more readable by excluding the last prompt maker
+            self.hoffset -= len(PROMPT_MARKER.strip('\r\n')) # include next prompt in the next read, but not the newlines
+            return h[:-len(PROMPT_MARKER)]
         return h
 
     def play(self, command: str) -> None:
         if command in self.disallowed:
             return
         self.process.sendline(command)
-        self._expect_multiple('> ')
         self._update_history()
 
     def _update_history(self):
+        self._read_all(PROMPT_MARKER)
         self.process.logfile_read.seek(self.poffset)
         t = self.process.logfile_read.read()
         t = _escape_ansi(t)
@@ -64,9 +65,9 @@ class AdventureGame:
         p.close()
         return _escape_ansi(content)
 
-    def _expect_multiple(self, pattern: str):
+    def _read_all(self, pattern):
         try:
-            while True:
+            while True: # multiple patterns may appear in the output; read until no more remain
                 self.process.expect(pattern, timeout=0.1)
         except pexpect.TIMEOUT:
             pass
